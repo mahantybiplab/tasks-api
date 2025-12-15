a2183bd38ed50a98998506cb0053ead6b7322789
biplab mahanty <bmahanty501@gmail.com>
2025-12-15 16:50:43 +0530

    feat: implement notFound handler with stoker middleware

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { OpenAPIHono } from '@hono/zod-openapi'
+import { notFound } from 'stoker/middlewares';
+
+const app = new OpenAPIHono()
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.notFound(notFound)
+export default app
---
+++ workflow.md
@@ -0,0 +1,16 @@
+1. antfu Eslint Setup & lint fix script
+
+```ts
+// 2.
+   "paths": {
+
+	"@/*":["./src/*"]
+	
+	}
+```
+   
+```shell
+# 3.
+pn add hono zod @hono/zod-openapi
+```
+
