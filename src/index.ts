c42a8ff820199290c773435a41a90a7b49acbfe0
biplab mahanty <bmahanty501@gmail.com>
2025-12-15 20:22:16 +0530

    feat: typesafe env with zod

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { serve } from "@hono/node-server";
 
 import app from "./app";
+import env from "./env";
 
 
 serve({
   fetch: app.fetch,
-  port: process.env.PORT || 3000,
+  port: env.PORT,
 }, (info) => {
   // eslint-disable-next-line no-console
   console.log(`Server is running on http://localhost:${info.port}`);
