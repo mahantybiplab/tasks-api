b9c3459fff3f31e5d27c10e703640d63f0f10a59
biplab mahanty <bmahanty501@gmail.com>
2025-12-16 11:28:12 +0530

    feat: simplify index route response schema using jsonContent and group route definitions with tags

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,27 +1,23 @@
-import { createRoute, z } from "@hono/zod-openapi";
+import { createRoute } from "@hono/zod-openapi";
+import * as HttpStatusCode from "stoker/http-status-codes";
+import { jsonContent } from "stoker/openapi/helpers";
+import { createMessageObjectSchema } from "stoker/openapi/schemas";
 
 import { createRouter } from "@/lib/create-app";
 
 const router = createRouter()
   .openapi(createRoute({
+    tags:["Index"],
     method: "get",
     path: "/",
     responses: {
-      200: {
-        content: {
-          "application/json": {
-            schema: z.object({
-              message: z.string(),
-            }),
-          },
-        },
-        description: "Tasks API Index",
-      },
+      [HttpStatusCode.OK]: jsonContent(createMessageObjectSchema("Tasks API"), "Tasks API Index"),
+
     },
   }), (c) => {
     return c.json({
       message: "Tasks API",
-    });
+    }, HttpStatusCode.OK);
   });
 
 export default router;
